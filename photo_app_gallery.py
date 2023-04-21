ce3cae4be9d7a9cafcfcde955681b3accd245832
petchorine <petchorine_tech@proton.me>
2023-04-21 14:54:10 +0200

    test (photoApp + Gallery) : rafraichir la galerie avec les photos prises par le photoApp

--- photo_app_gallery.py
+++ photo_app_gallery.py
@@ -2,6 +2,7 @@ import os
 
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.uix.camera import Camera
 from kivy.metrics import dp
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
@@ -41,34 +42,50 @@ class MyNewGallery(Screen):
         self.my_gallery_grid.clear_widgets()
         self.add_images_to_gallery()
 
-class MenuB(Screen):
-    pass
+class MyCamera(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+
+        self.camera = Camera(resolution=(350, 700), play=True)
+        self.add_widget(self.camera)
+
+        button = Button(text="Prendre une photo", size_hint=(1, 0.1))
+        button.bind(on_press=self.on_button_press)
+        self.add_widget(button)
+
+    def on_button_press(self, instance):
+        path = "/home/petchorine/Desktop/monPyhon/mes_projets_python/Double_view/Images"
+        # compte le nbre de fichier dans les dossier destination des photos
+        # permet de renommer chaque photo prise avec un nbr en plus
+        num_files = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
+        self.camera.export_to_png(f"/home/petchorine/Desktop/monPyhon/mes_projets_python/Double_view/Images/image{num_files + 1}.png")
 
 class MyScreenManager(ScreenManager):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         self.screen1 = MyNewGallery(name="screen1")
-        screen2 = MenuB(name="screen2")
+        screen2 = MyCamera(name="screen2")
 
         btn_to_screen2 = Button(text="to screen 2",
                                 size_hint = (None, None),
                                 size = (100,100),
                                 pos_hint = {"right": 1})
-        btn_to_screen2.bind(on_press=self.to_b)
+        btn_to_screen2.bind(on_press=self.to_camera)
         self.screen1.add_widget(btn_to_screen2)
 
         btn_to_gallery = Button(text="to gallery",
                                 size_hint=(None, None),
                                 size=(100, 100),
-                                pos_hint={"left": 1})
+                                pos_hint={"top": 1, "left": 1})
         btn_to_gallery.bind(on_press=self.to_gallery)
         screen2.add_widget(btn_to_gallery)
 
         self.add_widget(self.screen1)
         self.add_widget(screen2)
 
-    def to_b(self, *args):
+    def to_camera(self, *args):
         self.transition.direction = "left"
         self.current = "screen2"
 
